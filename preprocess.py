a8500574a4c371d2aa26505e8908c22a03032a91
tanatosuu <103896573+tanatosuu@users.noreply.github.com>
2022-10-13 00:34:37 +0900

    Update preprocess.py
    Reduce memory for preprocessing

--- preprocess.py
+++ preprocess.py
@@ -6,26 +6,26 @@ import pandas as pd
 #torch.cuda.set_device(1)
 #choose datasets
 
-dataset='ml_100k'
+dataset='gowalla'
 #ml_1m
 #user_size,item_size=6040,3952
 #ml_100k
 #user_size,item_size=943,1682
 #pinterest
-user_size,item_size=37501,9836
+#user_size,item_size=37501,9836
 #citeulike
 #user_size,item_size=5551,16981
 #gowalla
-#user_size,item_size=29858,40981
+user_size,item_size=29858,40981
 
 #the number of required features
-smooth_ratio=0.2
-rough_ratio=0.002
+smooth_ratio=0.1
+rough_ratio=0.0
 
 
 # Adj: adjacency matrix
 # size: the number of required features
-# argest: Ture (default) for k-largest (smoothed) and Flase for k-smallest (rough) eigenvalues
+# largest: Ture (default) for k-largest (smoothed) and Flase for k-smallest (rough) eigenvalues
 # niter: maximum number of iterations
 def cal_spectral_feature(Adj, size, type='user', largest=True, niter=5):
 	# params for the function lobpcg
@@ -51,7 +51,7 @@ def cal_spectral_feature(Adj, size, type='user', largest=True, niter=5):
 
 
 
-int main():
+
 # build rating matrix for datasets
 df_train=pd.read_csv(dataset+r'/train_sparse.csv')
 rate_matrix=torch.zeros(user_size,item_size).cuda()
@@ -62,66 +62,31 @@ for row in df_train.itertuples():
 np.save(r'./'+dataset+r'/'+'rate_sparse.npy',rate_matrix.cpu().numpy())
 
 #user degree and item degree
-user_degree=rate_matrix.sum(1)
-item_degree=rate_matrix.sum(0)
+D_u=rate_matrix.sum(1)
+D_i=rate_matrix.sum(0)
 
-D=torch.diag(user_degree)
-M=torch.diag(item_degree)
 
 #in the case any users or items have no interactions
-for i in range(len(user_degree)):
-	if D[i,i]!=0:
-		D[i,i]=1/D[i,i].sqrt()
+for i in range(user_size):
+	if D_u[i]!=0:
+		D_u[i]=1/D_u[i].sqrt()
 
-for i in range(len(item_degree)):
-	if M[i,i]!=0:
-		M[i,i]=1/M[i,i]
-
-
+for i in range(item_size):
+	if D_i[i]!=0:
+		D_i[i]=1/D_i[i].sqrt()
 
 
 
-L_u=D.mm(rate_matrix).mm(M).mm(rate_matrix.t()).mm(D)
+#(D_u)^{-0.5}*rate_matrix*(D_i)^{-0.5}
+rate_matrix=D_u.unsqueeze(1)*rate_matrix*D_i
 
-
-
-
-D=torch.diag(item_degree)
-M=torch.diag(user_degree)
-
-for i in range(len(item_degree)):
-	if D[i,i]!=0:
-		D[i,i]=1/D[i,i].sqrt()
-
-for i in range(len(user_degree)):
-	if M[i,i]!=0:
-		M[i,i]=1/M[i,i]
-
-L_i=D.mm(rate_matrix.t()).mm(M).mm(rate_matrix).mm(D)
-
-
-
-
-# The size of user-user and item-item adjacency matrix
-print(L_u.shape,L_i.shape)
-
-
-#clear memory
-del D,M, rate_matrix, user_degree, item_degree 
+#clear GPU
+del D_u, D_i 
 gc.collect()
 torch.cuda.empty_cache()
 
-
-
-print('step 1 finished.')
-
-
-########################################
-########################################
-#calcualte spectral features
-
-
-
+#user-user matrix
+L_u=rate_matrix.mm(rate_matrix.t())
 
 #smoothed feautes for user-user relations
 cal_spectral_feature(L_u,int(smooth_ratio*user_size),type='user', largest=True)
@@ -129,13 +94,13 @@ cal_spectral_feature(L_u,int(smooth_ratio*user_size),type='user', largest=True)
 if rough_ratio!=0:
 	cal_spectral_feature(L_u,int(rough_ratio*user_size),type='user',largest=False)
 
-
-
-#clear memory if not enough
-del L_u
+#clear GPU
+del L_u 
 gc.collect()
 torch.cuda.empty_cache()
 
+#item-item matrix
+L_i=rate_matrix.t().mm(rate_matrix)
 
 
 #smoothed feautes for item-item relations
@@ -143,6 +108,3 @@ cal_spectral_feature(L_i,int(smooth_ratio*item_size),type='item', largest=True)
 #rough feautes for item-item relations
 if rough_ratio!=0:
 	cal_spectral_feature(L_i,int(rough_ratio*item_size),type='item',largest=False)
-
-
-
